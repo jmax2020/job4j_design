97d192c2b243d5fdddb1a25317920f169315456e
jmax2020 <jmax2020@mail.ru>
2022-07-18 22:27:15 +0700

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,7 @@
 package ru.job4j.io;
 
+import jdk.jshell.EvalException;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +11,20 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("test");
-        List<Path> list =  search(start, p -> p.toFile().getName().endsWith(".js"));
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        List<Path> list =  search(start, p -> p.toFile().getName().endsWith(args[1]));
         list.forEach(System.out::println);
     }
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length < 2 || args[0].length() == 0 || args[1].length() == 0) {
+            throw new IllegalArgumentException("Can't find arguments!");
+        }
     }
 }

bf93d2f53081ebd3110074182fe7c7bbc9ebb3d5
jmax2020 <jmax2020@mail.ru>
2022-10-16 07:51:35 +0300

    4. JAXB. Преобразование XML в POJO. [#315063 #351481]

--- src/main/java/ru/job4j/serialization/json/MainXml.java
+++ src/main/java/ru/job4j/serialization/json/MainXml.java
@@ -2,8 +2,16 @@ package ru.job4j.serialization.json;
 
 import ru.job4j.serialization.java.Contact;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 public class MainXml {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException, IOException {
         final Man man = new Man(true, "Max",
                 new Contact(555, "777-77-77"),
                 new int[] {2004, 2009, 2013});
@@ -13,11 +21,29 @@ public class MainXml {
                         + "<man sex=\"true\" name=\"Max\">\n"
                         + "    <contact phone=\"777-77-77\"/>\n"
                         + "    <yearsOfEducation>\n"
-                        + "        <yearOfEducation>2004</status>\n"
-                        + "        <yearOfEducation>2009</status>\n"
-                        + "        <yearOfEducation>2013</status>\n"
+                        + "        <yearOfEducation>2004</yearOfEducation>\n"
+                        + "        <yearOfEducation>2009</yearOfEducation>\n"
+                        + "        <yearOfEducation>2013</yearOfEducation>\n"
                         + "    </yearsOfEducation>\n"
                         + "</person>";
         System.out.println(xmlMan);
+
+        JAXBContext context = JAXBContext.newInstance(Man.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String manJAXB;
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(man, writer);
+            manJAXB = writer.getBuffer().toString();
+            System.out.println(System.lineSeparator());
+            System.out.println(manJAXB);
+        }
+        Unmarshaller unmanshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(manJAXB)) {
+            Man rsl = (Man) unmanshaller.unmarshal(reader);
+            System.out.println(rsl);
+        }
+
+
     }
 }

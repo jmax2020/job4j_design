cb3c170b5ebcc0e119f1fb738a39e35961efc8e3
jmax2020 <75690153+jmax2020@users.noreply.github.com>
2022-11-05 09:42:22 +0300

    0. Select [#6861]

---
+++ query.sql
@@ -0,0 +1,16 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date) values ('cat', 3650, '01.01.1745');
+insert into fauna(name, avg_age, discovery_date) values ('dog', 5000, null);
+insert into fauna(name, avg_age, discovery_date) values ('tarakan', 250, null);
+insert into fauna(name, avg_age, discovery_date) values ('fish_shark', 10000, '01.01.1500');
+
+select name, avg_age from fauna where name like '%fish%';
+select name, avg_age from fauna where avg_age > 10000 and avg_age <21000;
+select name from fauna where avg_age is null;
+select * from fauna where discovery_date < '01.01.1950';

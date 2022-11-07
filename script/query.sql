ed90454d1ddc3f60c9029ea360359973b6229d29
jmax2020 <jmax2020@mail.ru>
2022-11-07 22:29:12 +0300

    0. Select [#6861 #358001]

--- script/query.sql
+++ script/query.sql
@@ -4,10 +4,10 @@ create table fauna (
     avg_age int,
     discovery_date date
 );
-insert into fauna(name, avg_age, discovery_date) values ('cat', 3650, '01.01.1745');
+insert into fauna(name, avg_age, discovery_date) values ('cat', 3650, '1745-01-01');
 insert into fauna(name, avg_age, discovery_date) values ('dog', 5000, null);
 insert into fauna(name, avg_age, discovery_date) values ('tarakan', 250, null);
-insert into fauna(name, avg_age, discovery_date) values ('fish_shark', 10000, '01.01.1500');
+insert into fauna(name, avg_age, discovery_date) values ('fish_shark', 10000, '1500-01-01');
 
 select name, avg_age from fauna where name like '%fish%';
 select name, avg_age from fauna where avg_age > 10000 and avg_age <21000;

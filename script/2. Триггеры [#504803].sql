8b4b034dd0ce9ee7062d3ddd57aa1634a62265b3
jmax2020 <jmax2020@mail.ru>
2022-11-14 23:09:28 +0300

    2. Триггеры [#504803]

---
+++ script/2. Триггеры [#504803].sql
@@ -0,0 +1,64 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create or replace function nalog()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price * 1.2
+        where id = (select id from inserted);
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger nalog_trigger_after
+    after insert on products
+	referencing new table as inserted
+    for each statement
+    execute procedure nalog();
+
+create or replace function nalog_row()
+	returns trigger as
+$$
+	BEGIN
+		update products
+		set price = price * 1.2
+		where id = new.id;
+		return NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger nalog_trigger_before
+	before insert on products
+	for each row
+	execute procedure nalog_row();
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function insert_history()
+	returns trigger as
+$$
+	BEGIN
+		insert into history_of_price select new.id, new.name, now(), new.price;
+		return NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger history_price
+	after insert on products
+	for each row
+	execute procedure insert_history();
